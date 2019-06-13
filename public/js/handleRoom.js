1843ffdf4912189b707d8f635f1362736a0359d7
myhed <capuchondu92@hotmail.fr>
2019-06-13 14:18:57 +0200

    [WBS-6] add method filterOneTypeElement and whatRoomContainerDisplay

--- public/js/handleRoom.js
+++ public/js/handleRoom.js
@@ -1,1 +1,28 @@
-
+const handleRooms = {
+  filterOneTypeElement: (HTMLelements, filteringCriterion) => {
+    return Array.from(HTMLelements).filter(HTMLelement => {
+      if (HTMLelement.className.split(" ").length === filteringCriterion) {
+        return HTMLelement;
+      }
+    });
+  },
+  whatRoomContainerDisplay: labels => {
+    if (!Array.isArray(labels)) {
+      return;
+    }
+    let container;
+    labels.forEach(label => {
+      label.addEventListener("click",e => {
+          if (typeof container !== "undefined") {
+            container.containerClicked.style.zIndex = "0";
+          }
+          const containerClicked = document.getElementsByClassName(
+            e.target.className + " container"
+          )[0];
+          containerClicked.style.zIndex = "10000";
+          container = { containerClicked, label: e.target };
+        },false);
+    });
+    return container;
+  }
+};
