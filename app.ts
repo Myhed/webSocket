10d9b06616b5ed20276934ccc4e370ea20674bcb
myhed <capuchondu92@hotmail.fr>
2019-06-13 14:05:36 +0200

    [WBS-1] change data structure of rooms variable

--- app.ts
+++ app.ts
@@ -6,11 +6,17 @@ import * as path from 'path'
 const app = express();
 const server = new http.Server(app)
 const chat = io(server)
-const rooms: String[] = ['general','divers','jeux']
+interface Room {
+    name: string
+    tag: string
+}
+const rooms: Room[] = [ {name:'general',tag:'/'},
+{name:'divers',tag:'/divers'},
+{name:'jeux',tag:'/jeux'}]
 
 app.use(express.static(path.join(__dirname, '/public')))
 
-app.get('/getAllRoom',(req,res) => {
+app.get('/getAllRoom', (req,res) => {
     res.send(JSON.stringify(rooms))
 })
 app.get('/',(req,res) => {
