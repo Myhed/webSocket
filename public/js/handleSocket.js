a582d028e442ccbe042cb55acbf6a552def1a7d2
Myhed <capuchondu92@hotmail.fr>
2019-06-16 19:10:37 +0200

    [WBS-24] rename chat variable on currentSocketChannel and refactor method connexionToChannel

--- public/js/handleSocket.js
+++ public/js/handleSocket.js
@@ -1,39 +1,46 @@
 const handleSocket = {
     host: 'http://localhost:8080',
-    currentChannel: '/',
+    currentChannel:'',
+    currentSocketChannel: '',
     allChannel:[],
-    currentChat: '',
     getHost: () => {
         return handleSocket.host
     },
     setCurrentChannel: (channelName) => {
-        handleSocket.channel = channelName
+        handleSocket.currentChannel = channelName
         return handleSocket
     },
     getCurrentChannel: () => {
-        return handleSocket.channel
+        return handleSocket.currentChannel
     },
     whichChannelToConnect: (channel,index) => {
         if(typeof handleSocket.allChannel[index] === "undefined"){
-            return handleSocket
-                .setCurrentChannel(channel.tag)
+            console.log("ça passe par ici !", channel)
+                return handleSocket.setCurrentChannel(channel.tag)
                 .connexionToChannel(channel)
         }else{
             console.log('vous êtes déjà connecter voicis le chat')
-            handleSocket.chat = handleSocket.allChannel[index]
-            return handleSocket.chat
+            handleSocket.whichSocketChannel(handleSocket.allChannel[index].chat)
+            console.log(handleSocket.getSocketChannel())
+            return handleSocket.getSocketChannel()
         }
     },
-    connexionToChannel: (channel = null) => {
-        if(channel === null){
-            return handleSocket.chat
-        }
-        handleSocket.chat = io(handleSocket.host+handleSocket.channel)
-        handleSocket.allChannel.push(Object.assign(channel,{chat:handleSocket.chat}))
-        return handleSocket
+    whichSocketChannel:(socketChannel) => {
+        handleSocket.currentSocketChannel = socketChannel
+    },
+    getSocketChannel(){
+        return handleSocket.currentSocketChannel
+    },
+    connexionToChannel: (channel) => {
+            const chat = io(handleSocket.host+handleSocket.currentChannel)
+            handleSocket.whichSocketChannel(chat)
+            handleSocket.chatInfo = channel
+            handleSocket.allChannel.push(Object.assign(channel,{chat}))
+            
+            return handleSocket.getSocketChannel()
     },
     numberClientInChannel: () => {
-        handleSocket.chat.on('numberUserInChannel',data => {
+        handleSocket.currentSocketChannel.on('numberUserInChannel',data => {
             console.log(data)
         })
     }
